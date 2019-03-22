cc01a8203316e26cabb03d7019f0f66a8064b256
mellongraig <mellongraig2@hotmail.com>
2019-03-21 23:07:44 -0300

    Detailed recipe activity created including adding recipe list

---
+++ RecipeAssignment/app/src/main/java/com/example/graig/recipeassignment/DetailRecipes.java
@@ -0,0 +1,13 @@
+package com.example.graig.recipeassignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailRecipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_recipes);
+    }
+}
