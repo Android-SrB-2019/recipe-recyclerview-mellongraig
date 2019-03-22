10038e1242fd7eb1ff6ab5ca16499d7e9d1da5f2
mellongraig <mellongraig2@hotmail.com>
2019-03-22 06:44:26 -0300

    Recipe assignment updated

--- RecipeAssignment/app/src/main/java/com/example/graig/recipeassignment/MainActivity.java
+++ RecipeAssignment/app/src/main/java/com/example/graig/recipeassignment/MainActivity.java
@@ -7,17 +7,71 @@ package com.example.graig.recipeassignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
+    //calls the variables globally
+    private LinkedList<Recipe> mRecipeList = new LinkedList<>();
     private RecyclerView mRecyclerView;
     private RecipeListAdapter mRecipeAdapter;
-    private RecyclerView.LayoutManager mRecipeLayoutManager;
 
+    /**
+     * onCreate method
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Get a handle to the RecyclerView.
+        mRecyclerView = findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mRecipeAdapter = new RecipeListAdapter(this, mRecipeList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mRecipeAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    /**
+     * Options menu creation
+     * @param menu
+     * @return true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * Item selected menu
+     * @param item
+     * @return true
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
